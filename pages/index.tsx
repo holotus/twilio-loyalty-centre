0c8af8246e8e2971779e8ae5e6f32d8b91a8ac38
yip <holotus@gmail.com>
2022-08-19 20:58:26 +0800

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,13 +27,51 @@ const Home: NextPage = () => {
         <meta name="viewport" content="initial-scale=1.0" />
       </Head>
 
-      <h1>Ecommerce.</h1>
+      <h1 className={styles.header}>Ecommerce.</h1>
 
+      <div className={styles.menuItem}>Loyalty Centre</div>
 
+      <div className={styles.featureContainer}>
+        <div className='header'>Specially for you {"Kristian"}.</div>
+        <div className='descriptions'>ⓘ Because: You browsed men's product 5 out of 10 times.</div>
+        <img></img>
+      </div>
 
-      <Toggle id='cheese-status' defaultChecked={checked} onChange={handleChecked} /><label htmlFor='cheese-status'>Adjacent label tag</label>
+      <div className={styles.featureContainer}>
+        <div>Join our newsletter</div>
+        <div>Subscribe to our newsletter to receive the latest news and products.</div>
+        <div>
+          <Toggle id='cheese-status' defaultChecked={checked} onChange={handleChecked} />
+          <label htmlFor='cheese-status'>Email</label>
+          <div>{"kristian@gmailx.com"}</div>
+        </div>
+        <div>
+          <Toggle id='cheese-status' defaultChecked={checked} onChange={handleChecked} />
+          <label htmlFor='cheese-status'>SMS</label>
+          <div>{"no record"}</div>
+        </div>
+        <div>
+          <Toggle id='cheese-status' defaultChecked={checked} onChange={handleChecked} />
+          <label htmlFor='cheese-status'>WhatsApp</label>
+          <div>{"no record"}</div>
+        </div>
+      </div>
 
+      <div className={styles.featureContainer}>
+        <div>My referral code</div>
+        <div>Scan to redeem swags.</div>
+        <img></img>
+      </div>
 
+      <div className={styles.featureContainer}>
+        <div>Contact personal shopper via WhatsApp</div>
+        <div>No assignment</div>
+      </div>
+
+      <div className={styles.featureContainer}>
+        <div>My orders</div>
+        <div>View my previous and current orders (demo only).</div>
+      </div>
 
     </div>
   )
